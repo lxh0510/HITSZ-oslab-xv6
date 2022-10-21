421cd8efd330d30f798c1e61c759c9ba90edeb1e
200111304 <200111304@comp1.hwccluster.labcenter>
2022-10-21 03:41:13 +0000

    lab3

--- kernel/bio.c
+++ kernel/bio.c
@@ -23,32 +23,30 @@
 #include "fs.h"
 #include "buf.h"
 
-#define NBUCKETS 13
+#define NBUCKETS 13               // 设置哈希桶的个数
+
 struct {
   struct spinlock lock[NBUCKETS];
   struct buf buf[NBUF];
- // struct spinlock hashlock;
   // Linked list of all buffers, through prev/next.
   // Sorted by how recently the buffer was used.
   // head.next is most recent, head.prev is least.
-  struct buf hashbucket[NBUCKETS]; // 每个哈希队列一个linked list及一个lock
+  struct buf hashbucket[NBUCKETS]; // 每个哈希队列一个linked list及一个lock，其linked list是一个双向链表
 } bcache;
 
 void
 binit(void)
 {
   struct buf *b;
-  //initlock(&bcache.hashlock, "bcache");
-  for(int i=0;i<NBUCKETS;i++)
+  for(int i=0;i<NBUCKETS;i++)                                  // 对每个哈希桶进行初始化
   {
-    initlock(&bcache.lock[i], "bcache");
-  // Create linked list of buffers
-    bcache.hashbucket[i].prev = &bcache.hashbucket[i];
+    initlock(&bcache.lock[i], "bcache");                       // 初始化哈希桶的锁
+  // Create linked list of buffers 
+    bcache.hashbucket[i].prev = &bcache.hashbucket[i];         // 初始化哈希桶的双向链表
     bcache.hashbucket[i].next = &bcache.hashbucket[i];
   }
-  for(b = bcache.buf; b < bcache.buf+NBUF; b++){
+  for(b = bcache.buf; b < bcache.buf+NBUF; b++){             // 将每个缓存块按照哈希函数分配到其对应哈希桶中
     int i = b->blockno%NBUCKETS;
-    b->timestamp=ticks;
     b->next = bcache.hashbucket[i].next;
     b->prev = &bcache.hashbucket[i];
     initsleeplock(&b->lock, "buffer");
@@ -65,66 +63,66 @@ static struct buf*
 bget(uint dev, uint blockno)
 {
   struct buf *b;
-  int bucketno = blockno % NBUCKETS;
-  acquire(&bcache.lock[bucketno]);
+  int bucketno = blockno % NBUCKETS;                       // 获取该缓存块对应哈希桶号
+  acquire(&bcache.lock[bucketno]);                         // 给该哈希桶加锁
 
   // Is the block already cached?
-  for(b = bcache.hashbucket[bucketno].next; b != &bcache.hashbucket[bucketno]; b = b->next){
-    if(b->dev == dev && b->blockno == blockno){
-      b->refcnt++;
-      release(&bcache.lock[bucketno]);
-      acquiresleep(&b->lock);
+  for(b = bcache.hashbucket[bucketno].next; b != &bcache.hashbucket[bucketno]; b = b->next){     // 遍历哈希桶中所有缓存块
+    if(b->dev == dev && b->blockno == blockno){                                                  // 若命中了cache
+      b->refcnt++;                                        // 该块被引用次数加一
+      release(&bcache.lock[bucketno]);                    // 释放哈希桶的锁
+      acquiresleep(&b->lock);                             // 给该缓存块加睡眠锁
       return b;
     }
   }
 
   // Not cached.
   // Recycle the least recently used (LRU) unused buffer.
- // acquire(&bcache.hashlock);
+  // 若未命中
+  // 首先在原来哈希桶中找空闲内存
 
-  for(b = bcache.hashbucket[bucketno].prev; b != &bcache.hashbucket[bucketno]; b = b->prev){
+  for(b = bcache.hashbucket[bucketno].prev; b != &bcache.hashbucket[bucketno]; b = b->prev){     
     if(b->refcnt == 0) {
       b->dev = dev;
       b->blockno = blockno;
       b->valid = 0;
       b->refcnt = 1;
-   //   release(&bcache.hashlock);
-      release(&bcache.lock[bucketno]);
-      acquiresleep(&b->lock);
+      release(&bcache.lock[bucketno]);                  // 释放哈希桶的锁
+      acquiresleep(&b->lock);                           // 给该缓存块加睡眠锁
       return b;
     }
   }
-  
-  for(int i=0;i<NBUCKETS;i++)
+
+  // 若原来哈希桶也没有，则从其他哈希桶中“偷取”空闲内存
+
+  for(int i=0;i<NBUCKETS;i++)                           // 遍历其他哈希桶
   {
     if(i!=bucketno){
-      acquire(&bcache.lock[i]);
-      for(b = bcache.hashbucket[i].prev; b != &bcache.hashbucket[i]; b = b->prev){
-        if(b->refcnt == 0) {
+      acquire(&bcache.lock[i]);                         // 给该哈希桶加锁
+      for(b = bcache.hashbucket[i].prev; b != &bcache.hashbucket[i]; b = b->prev){       // 遍历该哈希桶寻找内存块
+        if(b->refcnt == 0) {                            // 若找到空闲内存
           b->dev = dev;
           b->blockno = blockno;
           b->valid = 0;
           b->refcnt = 1;
 
-          b->next->prev=b->prev;
-          b->prev->next=b->next;                         //从原哈希桶中pop出来
+          b->next->prev=b->prev;                         //从原哈希桶中pop出来
+          b->prev->next=b->next;                        
 
 
           b->next = bcache.hashbucket[bucketno].next;    //push到以bucketno为key的哈希桶中
           b->prev = &bcache.hashbucket[bucketno];
           bcache.hashbucket[bucketno].next->prev = b;
           bcache.hashbucket[bucketno].next = b;
-      //    release(&bcache.hashlock);
-          release(&bcache.lock[i]);
-          release(&bcache.lock[bucketno]);
-          acquiresleep(&b->lock);
+          release(&bcache.lock[i]);                      // 释放该哈希桶加锁
+          release(&bcache.lock[bucketno]);               // 释放原哈希桶的锁
+          acquiresleep(&b->lock);                        // 给该缓存块加睡眠锁
           return b;
         }
       }
       release(&bcache.lock[i]);
     }
   }
- // release(&bcache.hashlock);
   release(&bcache.lock[bucketno]);
   panic("bget: no buffers");
 }
@@ -154,19 +152,20 @@ bwrite(struct buf *b)
 
 // Release a locked buffer.
 // Move to the head of the most-recently-used list.
+
 void
 brelse(struct buf *b)
 {
   if(!holdingsleep(&b->lock))
     panic("brelse");
 
-  releasesleep(&b->lock);
-  int bucketno = b->blockno%NBUCKETS;
-  acquire(&bcache.lock[bucketno]);
-  b->refcnt--;
-  if (b->refcnt == 0) {
+  releasesleep(&b->lock);                            // 释放该缓存块的睡眠锁
+  int bucketno = b->blockno%NBUCKETS;                // 获取该缓存块的哈希桶号
+  acquire(&bcache.lock[bucketno]);                   // 给该哈希桶加锁
+  b->refcnt--;                                       // 该块被引用次数减一
+  if (b->refcnt == 0) {                              // 若该缓存块成为了空闲内存
     // no one is waiting for it.
-    b->next->prev = b->prev;
+    b->next->prev = b->prev;                           // 将其移入空闲内存列表的头部
     b->prev->next = b->next;
     b->next = bcache.hashbucket[bucketno].next;
     b->prev = &bcache.hashbucket[bucketno];
@@ -174,12 +173,12 @@ brelse(struct buf *b)
     bcache.hashbucket[bucketno].next = b;
   }
   
-  release(&bcache.lock[bucketno]);
+  release(&bcache.lock[bucketno]);                 // 释放该哈希桶的锁
 }
 
 void
 bpin(struct buf *b) {
-  int bucketno = b->blockno%NBUCKETS;
+  int bucketno = b->blockno%NBUCKETS;               // 改为对缓存块所在哈希桶进行加锁解锁操作
   acquire(&bcache.lock[bucketno]);
   b->refcnt++;
   release(&bcache.lock[bucketno]);
@@ -187,7 +186,7 @@ bpin(struct buf *b) {
 
 void
 bunpin(struct buf *b) {
-  int bucketno = b->blockno%NBUCKETS;
+  int bucketno = b->blockno%NBUCKETS;               // 改为对缓存块所在哈希桶进行加锁解锁操作
   acquire(&bcache.lock[bucketno]);
   b->refcnt--;
   release(&bcache.lock[bucketno]);
--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -23,18 +23,20 @@ struct kmem{
   struct run *freelist;
 };
 
-struct kmem kmems[NCPU];
+struct kmem kmems[NCPU];     // 每个cpu拥有一个结构体kmems，其中包含各自的锁lock和空闲内存链表freelist
+
 void
 kinit()
 {
   for(int i=0;i<NCPU;i++){
-    initlock(&kmems[i].lock, "kmem");
+    initlock(&kmems[i].lock, "kmem");      // 将每个cpu的锁进行初始化
   }
   freerange(end, (void*)PHYSTOP);
 }
 
+// freerange为所有运行freerange的CPU分配空闲的内存
 void
-freerange(void *pa_start, void *pa_end)
+freerange(void *pa_start, void *pa_end)     
 {
   char *p;
   p = (char*)PGROUNDUP((uint64)pa_start);
@@ -57,18 +59,18 @@ kfree(void *pa)
   // Fill with junk to catch dangling refs.
   memset(pa, 1, PGSIZE);
 
-  // 获取cpuid
+  // 关闭中断并获取cpuid
   push_off();
   int cid=cpuid();
 
   r = (struct run*)pa;
 
-  acquire(&kmems[cid].lock);
-  r->next = kmems[cid].freelist;
+  acquire(&kmems[cid].lock);    
+  r->next = kmems[cid].freelist;   // 使用头插法将空闲内存插入空闲内存链表中
   kmems[cid].freelist = r;
   release(&kmems[cid].lock);
 
-  pop_off();
+  pop_off();             //打开中断
 }
 
 // Allocate one 4096-byte page of physical memory.
@@ -80,41 +82,41 @@ kalloc(void)
 {
   struct run *r;
 
-  // 获取cpuid
+  // 关闭中断并获取cpuid
   push_off();
   int cid=cpuid();
 
-  acquire(&kmems[cid].lock);
-  r = kmems[cid].freelist;
+  acquire(&kmems[cid].lock);   // 加锁 
+  r = kmems[cid].freelist;  // 获取cpuid为cid的cpu的空闲内存链表
 
   if(r)
-    kmems[cid].freelist = r->next;
+    kmems[cid].freelist = r->next;  // 如果空闲链表有空闲内存，则将链表中首个空闲内存弹出
   else
   {
-    int find=0;
-    for(int i=0;i<NCPU;i++) 
+    int find=0;                         // find用于标记是否在其他cpu的空闲内存链表找到了空闲内存
+    for(int i=0;i<NCPU;i++)             // 遍历每一个cpu
     {
       if(i==cid)    continue;
-      acquire(&kmems[i].lock);
-      r = kmems[i].freelist;
-      if(r)
+      acquire(&kmems[i].lock);          // 对该cpu上锁
+      r = kmems[i].freelist;            // 获取该cpu的空闲内存链表
+      if(r)                             // 如果空闲链表有空闲内存，则将链表中首个空闲内存弹出
       {
         kmems[i].freelist = r->next;
-        find=1;
-        release(&kmems[i].lock);
+        find=1;                         // find标记为1，表示找到了空闲内存 
+        release(&kmems[i].lock);        // 在return前需要解锁
         break;
       }
-      release(&kmems[i].lock);
+      release(&kmems[i].lock);         // 未找到空闲内存也需要解锁
     }
-    if(find==0){
-      release(&kmems[cid].lock);
-      pop_off();
+    if(find==0){                      // 若遍历所有cpu均未找到
+      release(&kmems[cid].lock);      // 将cid对应cpu解锁
+      pop_off();                      // 打开中断
       return 0;
     } 
   }
 
-  release(&kmems[cid].lock);
-  pop_off();
+  release(&kmems[cid].lock);         // 将cid对应cpu解锁
+  pop_off();                         // 打开中断
 
   if(r)
     memset((char*)r, 5, PGSIZE); // fill with junk
